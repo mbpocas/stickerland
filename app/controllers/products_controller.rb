4e696296414588a6fc290ae50fd8b110abfffe91
Juliana Bonardi <julicbonardi@hotmail.com>
2022-08-23 16:12:13 -0300

    Add Logo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,14 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
 
     if @product.save
       redirect_to product_path(@product)
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
   def edit
   end
