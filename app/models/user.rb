7bb8a3961de66161122e1e3605205cd151a3578a
Mauricio Pocas <mbpocas@hotmail.com>
2022-08-23 11:09:45 -0300

    Validações ok e Seed OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :orders, dependent: :destroy
+  validates :first_name, :last_name, :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_193848) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_235640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,10 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_193848) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "category"
-    t.string "stickers"
+    t.string "sticker"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
