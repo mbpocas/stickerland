213a104150d8679c12a755cdfb7289719ba98f0a
Juliana Bonardi <julicbonardi@hotmail.com>
2022-08-23 14:35:52 -0300

    Product create funcional - Controller e View

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,20 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    # if @product.save
+    #   redirect_to product_path(@product)
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
+
   def edit
   end
 
