1a277c70b71f60843069f5d62011a991afe226c2
Mauricio Pocas <mbpocas@hotmail.com>
2022-08-23 15:01:42 -0300

    Delete e show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,20 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  def show
+  end
+
   def edit
   end
 
   def update
     @product.update(product_params)
-    # redirect_to product_path(@product)
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to product_path, status: :see_other
   end
 
   private
